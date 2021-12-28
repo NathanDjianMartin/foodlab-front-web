56c7fcc2cf557ecb87a9df2c055298ba9db1cf8b
Margot <margot-georget@hotmail.fr>
2021-12-28 15:39:19 +0100

    Added loggerService to display error and success message, replaces errorService

--- src/app/components/recipe-category/recipe-category-list/recipe-category-list.component.ts
+++ src/app/components/recipe-category/recipe-category-list/recipe-category-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {RecipeCategory} from "../../../models/recipe-category/recipe-category";
 import {RecipeCategoryService} from "../../../services/recipe-category/recipe-category.service";
 import {Observable} from "rxjs";
-import {ErrorService} from "../../../services/error/error.service";
+import {LoggerService} from "../../../services/logger/logger.service";
 
 @Component({
   selector: 'app-recipe-category-list',
@@ -13,7 +13,7 @@ export class RecipeCategoryListComponent implements OnInit {
   recipeCategories!: Observable<RecipeCategory[]>;
 
   constructor(private recipeCategoryService: RecipeCategoryService,
-              private errorService: ErrorService) {
+              private loggerService: LoggerService) {
   }
 
   ngOnInit(): void {
@@ -26,7 +26,7 @@ export class RecipeCategoryListComponent implements OnInit {
           this.ngOnInit()
         },
         (error) => {
-          this.errorService.displayToats("This category corresponds to several recipes, you cannot delete it")
+          this.loggerService.displayError("This category corresponds to several recipes, you cannot delete it")
           console.log("oups")
         }
     );
--- src/app/components/recipe/tab-recipes-by-category/tab-recipes-by-category.component.ts
+++ src/app/components/recipe/tab-recipes-by-category/tab-recipes-by-category.component.ts
@@ -1,24 +1,22 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Ingredient} from "../../../models/ingredient/ingredient";
-import {IngredientService} from "../../../services/ingredient/ingredient.service";
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 import {RecipeCategory} from "../../../models/recipe-category/recipe-category";
 import {Recipe} from "../../../models/recipe/recipe";
 import {RecipeService} from "../../../services/recipe/recipe.service";
 import {Router} from "@angular/router";
-import {ErrorService} from "../../../services/error/error.service";
+import {LoggerService} from "../../../services/logger/logger.service";
 
 @Component({
   selector: 'app-tab-recipes-by-category',
   templateUrl: './tab-recipes-by-category.component.html',
   styleUrls: ['./tab-recipes-by-category.component.css']
 })
-export class TabRecipesByCategoryComponent implements OnInit {
+export class TabRecipesByCategoryComponent implements OnInit{
   @Input() category!: RecipeCategory;
   recipes!: Recipe[];
 
   constructor(private recipeService: RecipeService,
               private router: Router,
-              private errorService: ErrorService) {
+              private loggerService: LoggerService) {
   }
 
 
@@ -30,6 +28,23 @@ export class TabRecipesByCategoryComponent implements OnInit {
     }
   }
 
+  /*ngAfterViewInit(){
+    (function ($) {
+      $(function () {
+
+        //initialize all modals
+        $('.modal').modal();
+
+        //now you can open modal from code
+        $('#modal1').modal('open');
+
+        //or by click on trigger
+        $('.trigger-modal').modal();
+
+      }); //end of document ready
+    })(jQuery); //end of jQuery name space
+  }*/
+
   deleteRecipe(recipe: Recipe){
     this.recipeService.deleteRecipe(recipe.id!).subscribe(
         (data) => {
@@ -37,7 +52,7 @@ export class TabRecipesByCategoryComponent implements OnInit {
         },
         (error) => {
           //ne s'affiche pas puisque c'est recipe execution qui est present
-          this.errorService.displayToats("This recipe is present in other recipes as a sub-recipe, you cannot delete it");
+          this.loggerService.displayError("This recipe is present in other recipes as a sub-recipe, you cannot delete it");
     });
     this.router.navigate(["/recipes",])
   }
