e5614e6720c7f5973131933e1c586683fc7226f5
nathandjianmartin <ndmroot-git@yahoo.fr>
2022-01-07 13:54:04 +0100

    Fixed server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-app.use(express.static(`./dist/heroku-test-deployment`));
+app.use(express.static(`./dist/foodlab-front-web`));
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/heroku-test-deployment/'});
+    res.sendFile('index.html', {root: 'dist/foodlab-front-web/'});
 });
 app.listen(process.env.PORT || 8080);
