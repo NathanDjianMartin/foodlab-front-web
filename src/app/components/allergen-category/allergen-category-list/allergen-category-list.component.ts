a3ae7858c23159c877afeba00e167d857fb5b1e6
Margot <margot-georget@hotmail.fr>
2022-01-10 17:14:10 +0100

    Update add allergen category

--- src/app/components/allergen-category/allergen-category-list/allergen-category-list.component.ts
+++ src/app/components/allergen-category/allergen-category-list/allergen-category-list.component.ts
@@ -4,6 +4,7 @@ import {Observable} from "rxjs";
 import {AllergenCategory} from "../../../models/allergen-category/allergen-category";
 import {AllergenCategoryService} from "../../../services/allergen-category/allergen-category.service";
 import {LoggerService} from "../../../services/logger/logger.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-allergen-category-list',
@@ -11,25 +12,47 @@ import {LoggerService} from "../../../services/logger/logger.service";
   styleUrls: ['./allergen-category-list.component.css']
 })
 export class AllergenCategoryListComponent implements OnInit {
-  allergenCategories!: Observable<AllergenCategory[]>;
+  allergenCategories!: AllergenCategory[];
+  allergenCategoryFormGroup!: FormGroup;
 
   constructor(
       private allergenCategoryService : AllergenCategoryService,
-      public loggerService : LoggerService) { }
+      public loggerService : LoggerService,
+      private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.allergenCategories = this.allergenCategoryService.getAllAllergenCategories();
+    this.allergenCategoryService.getAllAllergenCategories().subscribe({
+      next: (allergenCategories) => {
+        this.allergenCategories = allergenCategories;
+      }
+    });
+    this.init();
+  }
+
+  init(){
+    this.allergenCategoryFormGroup = this.fb.group({
+      name:[null,[Validators.required]]
+    })
   }
 
   deleteAllergenCategory(allergenCategory: AllergenCategory) {
     this.allergenCategoryService.deleteAllergenCategory(allergenCategory.id!).subscribe(
         (data) => {
-          this.ngOnInit()
+          let index = this.allergenCategories.indexOf(allergenCategory);
+          this.allergenCategories.splice(index,1);
         },
         (error) => {
           this.loggerService.displayError("This category corresponds to several ingredient, you cannot delete it")
         }
     );
+  }
+
+  createAllergenCategory(){
+    let allergenCategory = new AllergenCategory(this.allergenCategoryFormGroup.get("name")?.value);
+    this.allergenCategoryService.createAllergenCategory(allergenCategory).subscribe((data) => {
+      this.allergenCategories.push(allergenCategory);
+      this.init();
+    });
   }
 
 }
